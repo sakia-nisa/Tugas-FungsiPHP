f0fed958fe578017a2c45d93caec5a777a7a58a9
sakia-nisa <109323447+sakia-nisa@users.noreply.github.com>
2022-08-23 10:16:19 +0700

    Add files via upload

---
+++ sakiatunisa-12RPL1-2760.php
@@ -0,0 +1,16 @@
+<?php
+//Sakiatun Nisa XII RPL 1 2760
+
+$panjang=15;
+$lebar=12;
+$tinggi=8;
+  $volume=$panjang*$lebar*$tinggi;
+  echo "Luas Volume Balok Adalah $volume cm3";
+
+  echo '</br>';
+
+  $r=14;
+  $t=15;
+  $volume=1/3*M_PI*$r*$r*$t;
+  echo "Luas Volume Krucut Adalah $volume cm3";
+?>
